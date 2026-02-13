537433849b32a9d2a689feace4ce14569426c22f
Marwynn Somridhivej <msomridhivej329@gmail.com>
2026-02-12 21:46:15 -0500

    added lock/unlock queue commands, deduplicated autocomplete code, refactor to use classes in queue package

--- cogs/queue_cog.py
+++ cogs/queue_cog.py
@@ -14,7 +14,6 @@ class QueueCog(commands.Cog):
         self.bot: Bot = bot
         self.queue_loc = f"{self.bot.config.data_loc}/queue"
         self.queue_manager = QueueManager(self)
-        self._clean_queues_file.start()
 
     async def cog_load(self):
         await self.reload()
@@ -22,14 +21,6 @@ class QueueCog(commands.Cog):
 
     async def reload(self) -> None:
         await self.queue_manager.reload()
-
-    @tasks.loop(hours=24.0)
-    async def _clean_queues_file(self):
-        await self.queue_manager.clean_queues_file()
-
-    @_clean_queues_file.before_loop
-    async def _wait_until_ready(self):
-        await self.bot.wait_until_ready()
 
     @app_commands.command(name="createqueue", description="Creates a new queue for a custom match")
     @app_commands.rename(queue_type="type")
@@ -49,7 +40,7 @@ class QueueCog(commands.Cog):
         except QueueAlreadyExists:
             msg = "A queue already exists with the specified name"
         except Exception as e:
-            raise e
+            msg = f"An error has occurred: {e}"
         finally:
             await interaction.response.send_message(msg)
 
@@ -57,14 +48,14 @@ class QueueCog(commands.Cog):
     @app_commands.describe(name="The name of the queue you are trying to delete")
     async def _delete_queue(self, interaction: discord.Interaction, name: str):
         try:
-            await self.queue_manager.delete_queue(name, interaction.user.id)
+            await self.queue_manager.delete_queue(interaction.guild_id, name, interaction.user.id)
             msg = f"Successfully deleted the queue \"{name}\""
         except QueueDoesNotExist:
             msg = f"No queue exists with the name \"{name}\""
         except NotQueueOwner:
-            msg = f"Unable to delete queue, as you are not its owner"
+            msg = "Unable to delete the specified queue, as you are not its owner"
         except Exception as e:
-            raise e
+            msg = f"An error has occurred: {e}"
         finally:
             await interaction.response.send_message(msg)
 
@@ -74,22 +65,24 @@ class QueueCog(commands.Cog):
         owned_queues = [
             name for name, entry in queues.items() if entry.owner_id == interaction.user.id
         ]
-        return [app_commands.Choice(name=choice, value=choice) for choice in owned_queues if current.lower() in choice]
+        return self.get_sorted_choices(owned_queues, current)
 
     @app_commands.command(name="joinqueue", description="Join an existing queue")
     @app_commands.describe(name="The name of the queue you are trying to join")
     async def _join_queue(self, interaction: discord.Interaction, name: str):
         try:
-            await self.queue_manager.join_user_to_queue(interaction.user.id, name)
-            msg = f"You successfully joined the queue"
+            await self.queue_manager.join_user_to_queue(interaction.guild_id, interaction.user.id, name)
+            msg = f"You successfully joined the queue \"{name}\""
         except QueueDoesNotExist:
             msg = "Unable to find a queue with the specified name"
         except AlreadyInQueue:
             msg = "You are already in the specified queue"
         except QueueIsFull:
             msg = "Unable to join the specified queue, as it is already full"
+        except QueueIsLocked:
+            msg = "Unable to join the specified queue, as it is locked"
         except Exception as e:
-            raise e
+            msg = f"An error has occurred: {e}"
         finally:
             await interaction.response.send_message(msg)
 
@@ -97,32 +90,84 @@ class QueueCog(commands.Cog):
     async def _join_queue_autocomplete(self, interaction: discord.Interaction, current: str) -> List[app_commands.Choice[str]]:
         queues = await self.queue_manager.get_all_queues(interaction.guild_id)
         joinable_queues = [
-            name for name, entry in queues.items() if interaction.user.id not in entry.players
+            name for name, entry in queues.items() if len(entry.players) < entry.max_players and interaction.user.id not in entry.players
         ]
-        return [app_commands.Choice(name=choice, value=choice) for choice in joinable_queues]
+        return self.get_sorted_choices(joinable_queues, current)
 
     @app_commands.command(name="leavequeue", description="Leave an existing queue")
     @app_commands.describe(name="The name of the queue you are trying to leave")
     async def _leave_queue(self, interaction: discord.Interaction, name: str):
         try:
-            await self.queue_manager.leave_user_from_queue(interaction.user.id, name)
-            msg = f"You successfully left the queue"
+            await self.queue_manager.leave_user_from_queue(interaction.guild_id, interaction.user.id, name)
+            msg = f"You successfully left the queue \"{name}\""
         except QueueDoesNotExist:
             msg = "Unable to find a queue with the specified name"
         except NotInQueue:
             msg = "You are not in the specified queue"
+        except QueueIsLocked:
+            msg = "Unable to leave the specified queue, as it is locked"
         except Exception as e:
-            raise e
+            msg = f"An error has occurred: {e}"
         finally:
             await interaction.response.send_message(msg)
 
     @_leave_queue.autocomplete("name")
     async def _leave_queue_autocomplete(self, interaction: discord.Interaction, current: str) -> List[app_commands.Choice[str]]:
         queues = await self.queue_manager.get_all_queues(interaction.guild_id)
-        queues_user_is_in = [
+        leaveable_queues = [
             name for name, entry in queues.items() if interaction.user.id in entry.players
         ]
-        return [app_commands.Choice(name=choice, value=choice) for choice in queues_user_is_in if current.lower() in choice.lower()]
+        return self.get_sorted_choices(leaveable_queues, current)
+
+    @app_commands.command(name="lockqueue", description="Lock an existing queue")
+    @app_commands.describe(name="The name of the queue you are trying to lock")
+    async def _lock_queue(self, interaction: discord.Interaction, name: str):
+        try:
+            await self.queue_manager.set_queue_lock_state(interaction.guild_id, interaction.user.id, name, True)
+            msg = f"Queue \"{name}\" has been locked"
+        except QueueDoesNotExist:
+            msg = "Unable to find a queue with the specified name"
+        except QueueLockStateError:
+            msg = "The specified queue is alreaedy locked"
+        except NotQueueOwner:
+            msg = "Unable to lock the specified queue, as you are not its owner"
+        except Exception as e:
+            msg = f"An error has occurred: {e}"
+        finally:
+            await interaction.response.send_message(msg)
+
+    @_lock_queue.autocomplete("name")
+    async def _lock_autocomplete(self, interaction: discord.Interaction, current: str) -> List[app_commands.Choice[str]]:
+        queues = await self.queue_manager.get_all_queues(interaction.guild_id)
+        lockable_queues = [
+            name for name, entry in queues.items() if entry.locked == False and interaction.user.id == entry.owner_id
+        ]
+        return self.get_sorted_choices(lockable_queues, current)
+
+    @app_commands.command(name="unlockqueue", description="Unlock an existing queue")
+    @app_commands.describe(name="The name of the queue you are trying to unlock")
+    async def _unlock_queue(self, interaction: discord.Interaction, name: str):
+        try:
+            await self.queue_manager.set_queue_lock_state(interaction.guild_id, interaction.user.id, name, False)
+            msg = f"Queue \"{name}\" has been unlocked"
+        except QueueDoesNotExist:
+            msg = "Unable to find a queue with the specified name"
+        except QueueLockStateError:
+            msg = "The specified queue is alreaedy unlocked"
+        except NotQueueOwner:
+            msg = "Unable to unlock the specified queue, as you are not its owner"
+        except Exception as e:
+            msg = f"An error has occurred: {e}"
+        finally:
+            await interaction.response.send_message(msg)
+
+    @_unlock_queue.autocomplete("name")
+    async def _unlock_autocomplete(self, interaction: discord.Interaction, current: str) -> List[app_commands.Choice[str]]:
+        queues = await self.queue_manager.get_all_queues(interaction.guild_id)
+        unlockable_queues = [
+            name for name, entry in queues.items() if entry.locked == True and interaction.user.id == entry.owner_id
+        ]
+        return self.get_sorted_choices(unlockable_queues, current)
 
     @app_commands.command(name="listqueue", description="List all queues with filters")
     @app_commands.rename(queue_type="type")
@@ -141,10 +186,10 @@ class QueueCog(commands.Cog):
             for name, entry in results.items():
                 additions.append(
                     "\n".join([
-                        f"**{name} - {entry.type}**",
+                        f"**{name} [{entry.type}]**",
                         f">  Players: {len(entry.players)}/{entry.max_players}",
                         f">  Created on {entry.created_date}",
-                        f">  Owner: {interaction.guild.get_member(entry.owner_id).name}"
+                        f">  Owner: {interaction.guild.get_member(entry.owner_id).display_name}",
                         f">  Locked: {"Yes" if entry.locked else "No"}"
                         "\n",
                     ])
@@ -153,9 +198,17 @@ class QueueCog(commands.Cog):
         except NoListResults:
             msg = "Could not find any queues matching specified criteria"
         except Exception as e:
-            raise e
+            msg = f"An error has occurred: {e}"
         finally:
             await interaction.response.send_message(msg)
+
+    @staticmethod
+    def get_sorted_choices(entries: List[str], current: str) -> List[app_commands.Choice[str]]:
+        choices = [
+            app_commands.Choice(name=choice, value=choice)
+            for choice in entries if current.lower() in choice.lower()
+        ]
+        return sorted(choices, key=lambda x: x.name)
 
 
 async def setup(bot):
