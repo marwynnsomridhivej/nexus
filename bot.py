f9018335b0a93f392496239deabf5b67559c30f9
Marwynn Somridhivej <msomridhivej329@gmail.com>
2026-02-18 00:45:53 -0500

    use provided Bot.run

--- bot.py
+++ bot.py
@@ -5,10 +5,10 @@ import discord
 from discord.ext import commands
 
 from config import Config
+from matchmanager import MatchManager
 from queuemanager import QueueManager
 
-intents = discord.Intents.all()
-config = Config()
+CRASH_ON_EXCEPTION = False
 
 
 class Bot(commands.Bot):
@@ -18,34 +18,56 @@ class Bot(commands.Bot):
             **kwargs
         )
         self.config = config
-        self.queue_manager: QueueManager = QueueManager(self.__loc("queue"))
+        self.queue_manager: QueueManager = QueueManager(self.config.data_loc)
+        self.match_manager: MatchManager = MatchManager(self.config.data_loc)
+
+        if (kwargs.get("debug", False)):
+            self.add_listener(self.__delete_dms, "on_ready")
 
     async def setup_hook(self):
+        # Initialise managers
         await self.queue_manager.load()
+        await self.match_manager.load()
+
+        # Load all cogs
+        for cog in self.config.cogs:
+            try:
+                await self.load_extension(cog)
+            except Exception as e:
+                if (CRASH_ON_EXCEPTION):
+                    raise e
+                print(f"Couldn't load {cog} | {e}")
+
+        # Sync slash commands
         guild = discord.Object(id=self.config.nexus_guild_id)
         # self.tree.clear_commands(guild=guild)
         self.tree.copy_global_to(guild=guild)
         await self.tree.sync(guild=guild)
 
-    def __loc(self, _dir: str) -> str:
-        return f"{self.config.data_loc}/{_dir}"
+    async def __delete_dms(self):
+        await self.wait_until_ready()
+        for user in self.users:
+            try:
+                if user.bot:
+                    print(f"Skipped bot {user.name}...")
+                    continue
 
+                if user.dm_channel is None:
+                    print(f"No DM Channel exists for user {user.name}, skipping")
+                    continue
 
-async def main(crash_on_exception: bool = False):
-    bot = Bot(config=config, intents=intents)
-    for cog in config.cogs:
-        try:
-            await bot.load_extension(cog)
-        except Exception as e:
-            if (crash_on_exception):
-                raise
-            print(f"Couldn't load {cog} | {e}")
-    try:
-        await bot.start(config.token)
-    except KeyboardInterrupt:
-        await bot.close()
+                print(f"Found DM Channel for user {user.name}")
+                async for message in user.dm_channel.history():
+                    if message.author.id == self.user.id:
+                        print(f"Deleting message {message.id}")
+                        await message.delete()
+            except Exception as e:
+                print(f"[{user.name}] {e}")
 
 
 if __name__ == "__main__":
-    should_crash = len(sys.argv) > 1 and sys.argv[1] == "debug"
-    asyncio.run(main(crash_on_exception=should_crash))
+    CRASH_ON_EXCEPTION = len(sys.argv) > 1 and sys.argv[1] == "debug"
+    config = Config()
+
+    bot = Bot(config, intents=discord.Intents.all(), debug=CRASH_ON_EXCEPTION)
+    bot.run(config.token)
