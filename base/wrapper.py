d796d2b01816c52dc951704c6f3d36f3a554ec5f
Marwynn Somridhivej <msomridhivej329@gmail.com>
2026-02-18 00:49:35 -0500

    abc with provided parse classmethod

---
+++ base/wrapper.py
@@ -0,0 +1,24 @@
+from abc import ABC, abstractmethod
+
+
+__all__ = (
+    "WrapperBase",
+)
+
+
+class WrapperBase(ABC):
+    @abstractmethod
+    def serialise(self) -> dict:
+        raise NotImplementedError
+
+    @classmethod
+    def parse(cls, data: dict):
+        """Classmethod to create an instance of the wrapper class from dict representation of data
+
+        Args:
+            data (dict): Dictionary representation of class data
+
+        Returns:
+            Type[WrapperBase]: Wrapper class instance that inherits from WrapperBase
+        """
+        return cls(data)
