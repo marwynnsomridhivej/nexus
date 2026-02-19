9ad123ab9c530d2324ae7b749528b4f3e7c6f758
Marwynn Somridhivej <msomridhivej329@gmail.com>
2026-02-19 17:33:22 -0500

    use statmanager and rework DM mass deletion debug function

--- bot.py
+++ bot.py
@@ -1,4 +1,3 @@
-import asyncio
 import sys
 
 import discord
@@ -7,6 +6,7 @@ from discord.ext import commands
 from config import Config
 from matchmanager import MatchManager
 from queuemanager import QueueManager
+from statsmanager import StatsManager
 
 CRASH_ON_EXCEPTION = False
 
@@ -20,6 +20,7 @@ class Bot(commands.Bot):
         self.config = config
         self.queue_manager: QueueManager = QueueManager(self.config.data_loc)
         self.match_manager: MatchManager = MatchManager(self.config.data_loc)
+        self.stats_manager: StatsManager = StatsManager(self.config.data_loc)
 
         if (kwargs.get("debug", False)):
             self.add_listener(self.__delete_dms, "on_ready")
@@ -46,6 +47,8 @@ class Bot(commands.Bot):
 
     async def __delete_dms(self):
         await self.wait_until_ready()
+
+        print("DM CHECKING STARTED")
         for user in self.users:
             try:
                 if user.bot:
@@ -53,16 +56,18 @@ class Bot(commands.Bot):
                     continue
 
                 if user.dm_channel is None:
-                    print(f"No DM Channel exists for user {user.name}, skipping")
-                    continue
-
-                print(f"Found DM Channel for user {user.name}")
+                    print(
+                        f"No DM Channel exists for user {user.name}, creating...")
+                    await user.create_dm()
+                else:
+                    print(f"Found DM Channel for user {user.name}")
                 async for message in user.dm_channel.history():
                     if message.author.id == self.user.id:
                         print(f"Deleting message {message.id}")
                         await message.delete()
             except Exception as e:
                 print(f"[{user.name}] {e}")
+        print("DM CHECKING FINISHED")
 
 
 if __name__ == "__main__":
