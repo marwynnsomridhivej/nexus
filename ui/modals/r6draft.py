190290d0bdf8e68593e6f1eb9fddd114948d877d
Marwynn Somridhivej <msomridhivej329@gmail.com>
2026-03-01 04:28:56 -0500

    auto draft last remaining players without need for captain input

--- ui/modals/r6draft.py
+++ ui/modals/r6draft.py
@@ -23,7 +23,7 @@ class R6DraftModal(discord.ui.Modal):
                     discord.SelectOption(
                         label=name,
                         value=_id
-                    ) for name, _id in self._r6view._get_draftable()
+                    ) for name, _id in self._r6view.draftable_players
                 ],
                 required=True,
             ),
@@ -47,7 +47,46 @@ class R6DraftModal(discord.ui.Modal):
         # Update local MatchEntry instance attached to R6View
         await self._r6view._update_match()
 
+        # Notify
         await interaction.response.send_message(
+            content=f"Captain <@{captain_id}> has drafted <@{drafted_id}>",
+            delete_after=10.0
+        )
+
+        # Check if there is one player left. If so, do the following actions:
+        # If playercount is:
+        #   EVEN    --> auto-draft to opposite side (higher elo captain)
+        #   ODD     --> auto-draft to same side (lower elo captain)
+        #
+        # If there is more than one player left, notify the other captain they can draft.
+        if len(self._r6view.draftable_players) > 1:
+            await interaction.channel.send(
+                content=f"*It is now <@{self._r6view.other_captain_id(captain_id)}>'s turn to draft*",
+                delete_after=10.0
+            )
+            return
+
+        # Last player to draft in draftable [name, id (in string form)]
+        drafted_id = int(self._r6view.draftable_players[0][1])
+
+        # Switch captain_id to the other captain if playercount is EVEN
+        # Do not switch if playercount is ODD, otherwise higher elo captain is on the team +1
+        if self._r6view.playercount % 2 == 0:
+            captain_id = self._r6view.other_captain_id(captain_id)
+
+        # Draft the last remaining player
+        await self._r6view._bot.match_manager.draft_player(
+            interaction.guild_id,
+            self._r6view._payload.match_name,
+            captain_id,
+            drafted_id
+        )
+
+        # Update local MatchEntry instnace attached to R6View
+        await self._r6view._update_match()
+
+        # Notify
+        await interaction.channel.send(
             content=f"Captain <@{captain_id}> has drafted <@{drafted_id}>",
             delete_after=10.0
         )
