5728961f17b1debf7a795f672ded19c6da3c6958
Marwynn Somridhivej <msomridhivej329@gmail.com>
2026-03-02 02:18:26 -0500

    reintroduce nuclear DM delete option for cleanup and fix encoding issues with logfiles

--- bot.py
+++ bot.py
@@ -32,6 +32,9 @@ class Bot(commands.Bot):
         self.queue_manager: QueueManager = QueueManager(self.config.data_dir)
         self.stats_manager: StatsManager = StatsManager(self.config.data_dir)
 
+        # Should we wipe DMs with everyone?
+        self.dm_wipe = kwargs.get("dm_wipe")
+
     async def setup_hook(self):
         # Initialise managers
         await self.dm_manager.load()
@@ -57,10 +60,36 @@ class Bot(commands.Bot):
         await self.wait_until_ready()
         await self.dm_manager.purge_all()
 
+        if not self.dm_wipe:
+            return
+
+        # Delete any stragglers
+        for user in self.users:
+            # Don't do bots
+            if user.bot:
+                continue
+
+            deleted_ids = []
+            dm_channel = await user.create_dm()
+            async for message in dm_channel.history(limit=None):
+                # Don't delete messages we aren't the author of
+                if message.author.id != self.user.id:
+                    continue
+
+                await message.delete()
+                deleted_ids.append(message.id)
+                self.logger.info(
+                    f"Deleted message ID {message.id} for user {user.display_name} ({user.id}) in DMs")
+
+            if not deleted_ids:
+                self.logger.info(
+                    f"Did not find any deletable DMs with user {user.display_name} ({user.id})")
+
 
 if __name__ == "__main__":
     # Check if debug flag is set
-    debug = len(sys.argv) > 1 and sys.argv[1].lower() == "debug"
+    debug = len(sys.argv) > 1 and "debug" in sys.argv
+    dm_wipe = len(sys.argv) > 1 and "wipe" in sys.argv
 
     # Load bot config from disk
     config = Config()
@@ -74,6 +103,7 @@ if __name__ == "__main__":
         filename=f"{config.log_dir}/{datetime.now().strftime("%d-%m-%Y_%H-%M-%S")}.log",
         maxBytes=16 * 1024 * 1024,  # 16 MiB
         backupCount=10,
+        encoding="utf-8",
     )
     stdout_handler = logging.StreamHandler(sys.stdout)
     stdout_handler.setLevel(logging.INFO)
@@ -90,5 +120,5 @@ if __name__ == "__main__":
         logger.addHandler(handler)
 
     # Run bot
-    bot = Bot(config, intents=discord.Intents.all())
+    bot = Bot(config, intents=discord.Intents.all(), dm_wipe=dm_wipe)
     bot.run(config.token, log_handler=None)
