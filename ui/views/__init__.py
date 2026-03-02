2c7d3e6deb72f3676ee7bce465ed2d35bf0420fc
Marwynn Somridhivej <msomridhivej329@gmail.com>
2026-03-02 02:06:23 -0500

    add MatchStartDMView to __all__

--- ui/views/__init__.py
+++ ui/views/__init__.py
@@ -1,10 +1,12 @@
 __all__ = (
     "LeaderboardView",
+    "MatchStartDMView",
     "QueueListView",
     "R6View",
 )
 
 
 from .leaderboard import LeaderboardView
+from .match_start_dm import MatchStartDMView
 from .queue_list import QueueListView
 from .r6 import R6View
