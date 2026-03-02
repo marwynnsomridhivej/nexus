0ed8a05e380677075cbf3cfd1a1e5d36e6a0d2d5
Marwynn Somridhivej <msomridhivej329@gmail.com>
2026-03-02 02:04:43 -0500

    dm manager helps bot keep track of DMs that should be deleted

---
+++ dmmanager/dm.py
@@ -0,0 +1,28 @@
+from typing import Dict, Tuple, Union
+
+from base import WrapperBase
+
+
+class DMWrapper(WrapperBase):
+    __slots__ = (
+        "data",
+    )
+
+    def __init__(self, data: Dict[str, int]):
+        self.data = data
+
+    @staticmethod
+    def _key(guild_id: int, user_id: int) -> str:
+        return f"{guild_id}_{user_id}"
+
+    def get(self, guild_id: int, user_id: int) -> Union[int, None]:
+        return self.data.get(self._key(guild_id, user_id))
+
+    def create(self, guild_id: int, user_id: int, message_id: int) -> None:
+        self.data[self._key(guild_id, user_id)] = message_id
+
+    def delete(self, guild_id: int, user_id: int) -> int:
+        return self.data.pop(self._key(guild_id, user_id))
+
+    def serialise(self) -> dict:
+        return self.data
---
+++ dmmanager/dm_manager.py
@@ -0,0 +1,56 @@
+import discord
+
+from base import ManagerBase
+
+from .dm import DMWrapper
+
+
+class DMManager(ManagerBase):
+    def __init__(self, dm_loc: str, bot):
+        super().__init__(dm_loc, "dms")
+
+        from bot import Bot
+        self.bot: Bot = bot
+
+    async def load(self):
+        await super().load(name="DMManager")
+
+    async def _get_or_create_wrapper(self) -> DMWrapper:
+        return await super()._get_or_create_wrapper(cls=DMWrapper)
+
+    async def purge_all(self) -> None:
+        wrapper = await self._get_or_create_wrapper()
+        for identifier, message_id in wrapper.data.items():
+            guild_id, user_id = [int(_id) for _id in identifier.split("_")]
+            dm_channel = await self.bot.get_user(user_id).create_dm()
+            try:
+                await dm_channel.get_partial_message(message_id).delete()
+                self.bot.logger.info(
+                    f"Deleted message ID {message_id} for user {user_id}")
+            except discord.NotFound:
+                self.bot.logger.info(
+                    f"Message ID {message_id} for user {user_id} was already deleted")
+            except discord.HTTPException as e:
+                self.bot.logger.error(
+                    f"HTTPException when trying to delete message ID {message_id} for user {user_id}: {e}")
+            except Exception as e:
+                self.bot.logger.error(
+                    "An exception occurred when trying to delete " +
+                    f"message ID {message_id} for user {user_id}: {e}"
+                )
+        wrapper.data = {}
+        await self.write(wrapper)
+
+    async def create(self, guild_id: int, user_id: int, message_id: int) -> None:
+        wrapper = await self._get_or_create_wrapper()
+        if wrapper.get(guild_id, user_id) is not None:
+            raise KeyError(
+                f"Entry already exists for guild_id {guild_id} and user_id {user_id}")
+        wrapper.create(guild_id, user_id, message_id)
+        await self.write(wrapper)
+
+    async def delete(self, guild_id: int, user_id: int) -> int:
+        wrapper = await self._get_or_create_wrapper()
+        message_id = wrapper.delete(guild_id, user_id)
+        await self.write(wrapper)
+        return message_id
