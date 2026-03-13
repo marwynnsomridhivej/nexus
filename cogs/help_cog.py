9c8f4293188fba5775fe927bd103905b58faffce
Marwynn Somridhivej <msomridhivej329@gmail.com>
2026-03-13 13:40:54 -0400

    add help command placeholder

---
+++ cogs/help_cog.py
@@ -0,0 +1,26 @@
+import discord
+from discord import app_commands
+from discord.ext import commands
+
+
+HELP_URL = "https://github.com/marwynnsomridhivej/nexus/blob/main/README.md#getting-started"
+
+
+class HelpCog(commands.Cog):
+    def __init__(self, bot):
+        from bot import Bot
+        self.bot: Bot = bot
+
+    async def cog_load(self):
+        self.bot.logger.info("[HelpCog] Successfully loaded")
+
+    @app_commands.command(name="help", description="Displays command information")
+    async def _help_command(self, interaction: discord.Interaction):
+        await interaction.response.send_message(
+            f"Please refer to [this page]({HELP_URL}) for detailed command information",
+            ephemeral=True,
+        )
+
+
+async def setup(bot):
+    await bot.add_cog(HelpCog(bot))
